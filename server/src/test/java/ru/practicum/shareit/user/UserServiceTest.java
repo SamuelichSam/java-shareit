4121aac5f0f7f972db06b188e4d9114e5be94f8f
SamuelichSam <samuelsamuelichsemenov@yandex.ru>
2025-04-26 18:20:37 +0300

    ФЗ 16го спринта 1-я итерация

--- server/src/test/java/ru/practicum/shareit/user/UserServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.exception.ConflictException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
